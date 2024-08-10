bcc3790215bf87c8ae6208dfefc321f48b075465
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-08-10 20:16:16 +0200

    added callbacks and default values

--- src/componets/topbar.tsx
+++ src/componets/topbar.tsx
@@ -2,6 +2,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type props = {
   currentColor: string;
+  currentPixel: number;
+  onSettingsPressed?: () => void;
+  onChooseColor?: (color: string) => void;
+  onPixelChange?: (pixels: string) => void;
 };
 
 const TopBar = (props: props) => {
@@ -9,10 +13,18 @@ const TopBar = (props: props) => {
 
   return (
     <div id="topBar">
-      <input type="color" className="colorPicker" />
-      <input></input>
-      <div style={{ flex: 1 }}></div>
-      <button style={{ marginRight: 10 }}>
+      <input
+        type="color"
+        className="colorPicker"
+        value={props.currentColor}
+        onChange={(event) => props.onChooseColor?.(event.target.value)}
+      />
+      <input
+        value={props.currentPixel}
+        onChange={(event) => props.onPixelChange?.(event.target.value)}
+      />
+      <div style={{ flex: 1 }} />
+      <button style={{ marginRight: 10 }} onClick={props.onSettingsPressed}>
         <FontAwesomeIcon icon="gear" color="black" fontSize={20} />
       </button>
     </div>
