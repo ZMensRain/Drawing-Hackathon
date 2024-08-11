d9e21d271956e2e06be6e26cbaf6edcac8a413f4
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-08-11 16:48:43 +0200

    created settings type

---
+++ src/models/settings.ts
@@ -0,0 +1,11 @@
+type settings = {
+  theme: "light" | "dark";
+  brushSize: number;
+  brushColor: string;
+  tool: number;
+  dysfunctionalRange: number[];
+  backgroundColor: string;
+  popups: boolean;
+};
+
+export type { settings };
