ec2d8c6a2fabfea46f1931d435d136d1ce76f31b
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-08-15 13:13:57 +0200

    updated to have a drawing function

--- src/components/drawingArea.tsx
+++ src/components/drawingArea.tsx
@@ -1,27 +1,42 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 type props = { backgroundColor: string };
 
 const CanvasDrawing = (props: props) => {
-  const [pixelDimensions] = useState({ width: 100, height: 100 });
-
-  window.addEventListener("load", () => {
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    if (canvas == null) return;
-    canvas.width = canvas.scrollWidth;
-    canvas.height = canvas.scrollHeight;
-  });
-
-  // Feature not bug draws background over drawings when background color changes
-  // TODO test that this only gets called when background color changes
-  useEffect(() => {
+  const updateBackgroundColor = () => {
+    console.log("called");
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     if (canvas == null) return;
     const ctx = canvas.getContext("2d");
     if (ctx == null) return;
     ctx.fillStyle = props.backgroundColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, [props.backgroundColor]);
+  };
+
+  const drawingEntryPoint = (x: number, y: number) => {
+    console.log(x, y);
+  };
+
+  window.addEventListener("load", () => {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    if (canvas == null) return;
+    canvas.width = canvas.scrollWidth;
+    canvas.height = canvas.scrollHeight;
+
+    canvas.addEventListener("mousemove", function (e) {
+      const cRect = canvas.getBoundingClientRect();
+      const canvasX = Math.round(e.clientX - cRect.left);
+      const canvasY = Math.round(e.clientY - cRect.top);
+
+      if (e.buttons == 1) {
+        drawingEntryPoint(canvasX, canvasY);
+      }
+    });
+  });
+
+  const [pixelDimensions] = useState({ width: 100, height: 100 });
+  // Feature not bug draws background over drawings when background color changes
+  updateBackgroundColor();
 
   return (
     <canvas
